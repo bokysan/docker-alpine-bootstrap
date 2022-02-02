72f3d36a01a338882db831096a1886af4ef3d350
Bojan Čekrlić <bojan@cekrlic.com>
2022-02-02 12:32:58 +0100

    Update: Fix building of amd64 images

--- scripts/download-kubectl.sh
+++ scripts/download-kubectl.sh
@@ -4,7 +4,11 @@ CURL_OPTS="-s --retry 5 --max-time 300 --connect-timeout 10 -fSL"
 KUBECTL_VERSION=$(curl $CURL_OPTS https://dl.k8s.io/release/stable.txt)
 ARCH="$(uname -m)"
 OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
-if test "${ARCH}" == "aarch64"; then ARCH=arm64; fi
+if test "${ARCH}" == "aarch64"; then 
+    ARCH="arm64"
+elif test "${ARCH}" == "x86_64"; then 
+    ARCH="amd64"
+fi
 URL="https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/${OS}/${ARCH}/kubectl"
 echo "$URL"
 curl $CURL_OPTS -o /tmp/kubectl "${URL}"
